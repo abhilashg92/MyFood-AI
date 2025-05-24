3881322a1cc9e576ced5a5bc850bc7ad294f8933
abhilashg92 <ghogaleabhilash92@gmail.com>
2025-05-24 19:43:38 +0530

    Initial Commit

---
+++ MyFood-AI/MyFood_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFood_AIApp.swift
+//  MyFood-AI
+//
+//  Created by Abhilash Ghogale on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFood_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
